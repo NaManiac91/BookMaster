0046889e3eac4b74233412a6701fb7ebd255b8c9
NaManiac91 <ge.costantino91@gmail.com>
2024-11-20 10:58:55 +0100

    Update ProviderRepositoryInterface.java
    test push 1

--- src/main/java/com/example/book/BookMaster/repo/ProviderRepositoryInterface.java
+++ src/main/java/com/example/book/BookMaster/repo/ProviderRepositoryInterface.java
@@ -7,32 +7,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.book.BookMaster.models.Provider;
 
+@RestResource(exported = false)
 public interface ProviderRepositoryInterface extends CrudRepository<Provider, UUID> {
-	@Override
-	@RestResource(exported = false)
-	<S extends Provider> S save(S entity);
 	
-	@Override
-	@RestResource(exported = false)
-	<S extends Provider> Iterable<S> saveAll(Iterable<S> entities);
-	
-	@Override
-	@RestResource(exported = false)
-	void deleteById(UUID id);
-	
-	@Override
-	@RestResource(exported = false)
-	 void delete(Provider entity);
-	
-	@Override
-	@RestResource(exported = false)
-	void deleteAllById(Iterable<? extends UUID> ids);
-	
-	@Override
-	@RestResource(exported = false)
-	void deleteAll(Iterable<? extends Provider> entities);
-	
-	@Override
-	@RestResource(exported = false)
-    void deleteAll();
 }
