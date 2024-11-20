b441c95cf9218cba6a18e07fd44cae559ab5faab
NaManiac91 <ge.costantino91@gmail.com>
2024-11-20 11:00:28 +0100

    Update ReservationRepositoryInterface.java
    test dev branch

--- src/main/java/com/example/book/BookMaster/repo/ReservationRepositoryInterface.java
+++ src/main/java/com/example/book/BookMaster/repo/ReservationRepositoryInterface.java
@@ -7,32 +7,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.book.BookMaster.models.Reservation;
 
+@RestResource(exported = false)
 public interface ReservationRepositoryInterface extends CrudRepository<Reservation, UUID> {
-	@Override
-	@RestResource(exported = false)
-	<S extends Reservation> S save(S entity);
 	
-	@Override
-	@RestResource(exported = false)
-	<S extends Reservation> Iterable<S> saveAll(Iterable<S> entities);
-	
-	@Override
-	@RestResource(exported = false)
-	void deleteById(UUID id);
-	
-	@Override
-	@RestResource(exported = false)
-	 void delete(Reservation entity);
-	
-	@Override
-	@RestResource(exported = false)
-	void deleteAllById(Iterable<? extends UUID> ids);
-	
-	@Override
-	@RestResource(exported = false)
-	void deleteAll(Iterable<? extends Reservation> entities);
-	
-	@Override
-	@RestResource(exported = false)
-    void deleteAll();
 }
